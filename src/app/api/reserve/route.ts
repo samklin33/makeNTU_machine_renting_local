40e111ac29b4eb3c65ffa06faadf5ab90fb6f970
YBA857142 <grcgs2601@gmail.com>
2023-12-30 12:28:09 +0800

    small fix

--- src/app/api/reserve/route.ts
+++ src/app/api/reserve/route.ts
@@ -1,16 +1,18 @@
 import prisma from "../../../../prisma/client"
 import { type NextApiRequest, NextApiResponse } from "next";
 
-export async function CreateReserve(req: NextApiRequest, res: NextApiResponse) {
-    console.log("req.body", req.body);
-    const { username, type, title, note } = req.body;
+//POST
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+    console.log("req.body is ", req.body);
+    const { group, type, filename, comment } = req.body;
+    console.log(group, type, filename, comment);
     try {
         const user = await prisma.request.create({
             data: {
-                group: username,
+                group: group,
                 type: type,
-                filename: title,
-                comment: note,
+                filename: filename,
+                comment: comment,
                 number: 0,
                 status:"pending",
             }
@@ -23,11 +25,13 @@ export async function CreateReserve(req: NextApiRequest, res: NextApiResponse) {
     }
 }
 
-export async function GetReserve (req: NextApiRequest, res:NextApiResponse) {
+//GET
+export async function GET (req: NextApiRequest, res:NextApiResponse) {
     const resultReq = await prisma.request.findMany();
     return res.status(200).json(resultReq);
 }
 
+//POST
 export const UpdateStatus = async (req: NextApiRequest, res: NextApiResponse) => {
     const { id, username, type, title, note, number, status} = req.body;
     try{
